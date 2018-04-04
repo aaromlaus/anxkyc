2e5d09898a1312aa7410c750a8a8a2d85cfba27b
turlahector <turlahector@gmail.com>
2018-04-05 04:02:22 +0800

    initial commit for welcome page

--- src/main/java/com/anx/kyc/springboot/web/KycWebApplication.java
+++ src/main/java/com/anx/kyc/springboot/web/KycWebApplication.java
@@ -2,13 +2,20 @@ package com.anx.kyc.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.anx.kyc")
-public class KycWebApplication {
+public class KycWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KycWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(KycWebApplication.class, args);
 	}
 
