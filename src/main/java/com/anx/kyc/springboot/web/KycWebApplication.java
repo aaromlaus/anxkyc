b7d294eac5fe18fd0cd86fbb9dc5a44399e09506
turlahector <turlahector@gmail.com>
2018-04-05 07:24:20 +0800

    Configure hibernate

--- src/main/java/com/anx/kyc/springboot/web/KycWebApplication.java
+++ src/main/java/com/anx/kyc/springboot/web/KycWebApplication.java
@@ -2,12 +2,16 @@ package com.anx.kyc.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.anx.kyc")
+@EnableJpaRepositories("com.anx.kyc.repository")
+@EntityScan("com.anx.kyc.model")
 public class KycWebApplication extends SpringBootServletInitializer {
 
 	@Override
